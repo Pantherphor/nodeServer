cc1db17e816bccf51f432dbbffcaf15dab547f4e
doobzin <34076447+doobzin@users.noreply.github.com>
2020-12-07 20:11:04 +0200

    adding configuration file

---
+++ config.js
@@ -0,0 +1,28 @@
+// this creates nad exports the configuration variables
+
+const { type } = require("os");
+const { env } = require("process");
+
+//container for all the environments
+var environments = {};
+
+//staging default
+environments.staging={
+    'port' : 3000,
+    'envName' : 'staging'
+};
+
+//production environment
+environments.production = {
+    'port' : 5000,
+    'envName' : 'production'
+};
+
+//determine whick env was passes as a commmant-line 
+var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+// check tahat the current enviroment is one of the environments above, if not, default to stating
+var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+//export the module
+module.exports = environmentToExport;
